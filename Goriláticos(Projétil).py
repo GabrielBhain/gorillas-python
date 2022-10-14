717ea41cefbaf457ccf68cfceaebd21226030f6a
Ar74ur <arthur.almeidas@upe.br>
2022-10-14 17:50:09 -0300

    Projétil (Ajustado)

--- Goriláticos(Projétil).py
+++ Goriláticos(Projétil).py
@@ -14,37 +14,32 @@ def parab(stdscr, angulo1, velocidade1):
     alcance = ((velocidade1 **2) * np.sin(2*theta)) /g
     x = 0
     t = 0
+    x_mao_macaco = 4
+    y_mao_macaco = 0
     loop_coord = 0
 
     while(loop_coord == 0):
         if(x < w):
-            #vy = (velocidade1 * np.sin(theta)) + g*t
-            x = 4 + int(((velocidade1*t) * np.cos(theta))) 
-            y = int(h - ((velocidade1*t) * np.sin(theta))-((0.5*g)*(t**2)))
+            vy = (velocidade1 * np.sin(theta)) - g*t
+            x = x_mao_macaco + int(((velocidade1*t) * np.cos(theta))) 
+            y = y_mao_macaco + int(h - ((vy*t))-((0.5*g)*(t**2)))
             
             try:
                 stdscr.clear()
-                stdscr.addstr(y, x, "O", curses.color_pair(3))
+                stdscr.addstr(y, x, "💣", curses.color_pair(3))
+                #Checar posições x e y:
+                #stdscr.addstr(10, 10, f"X: {x}")
+                #stdscr.addstr(10, 20, f"Y: {y}")
                 stdscr.refresh()
             except curses.error:
                 stdscr.clear()
                 stdscr.refresh()
                 pass
 
-            if(t > t_subida):
-                if(g == 9.8):
-                    g = -9.8
-                    
-            elif(x > w):
-                loop_coord = 1
-            
-            #Checar posições x e y:
-            #stdscr.addstr(10, 10, f"X: {x}")
-            #stdscr.addstr(10, 20, f"Y: {y}")
             t += 0.1
-            time.sleep(0.5)
-            
-        elif(y > h):
+            time.sleep(0.1)
+                        
+        elif(y > h or x > w):
             loop_coord = 1
             break    
         else:
@@ -57,7 +52,7 @@ def traj(stdscr):
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_RED)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_YELLOW)
     angulo1 = 10
     velocidade1 = 20
     loop_ang = 1
@@ -65,7 +60,7 @@ def traj(stdscr):
     stdscr.clear()
 
     while(loop_ang != 0):
-        stdscr.addstr(10, 0, f"Ângulo: {angulo1}", curses.color_pair(2))
+        stdscr.addstr(5, 3, f"Ângulo: {angulo1}", curses.color_pair(1))
         key = stdscr.getch()
 
         if key == curses.KEY_UP and angulo1 < 85:
@@ -77,7 +72,7 @@ def traj(stdscr):
             loop_vel = 0
 
             while(loop_vel == 0):
-                stdscr.addstr(12, 0, f"Velocidade: {velocidade1}", curses.color_pair(2))
+                stdscr.addstr(7, 3, f"Velocidade: {velocidade1}", curses.color_pair(1))
                 key = stdscr.getch()
                 
                 if key == curses.KEY_UP and velocidade1 < 50:
